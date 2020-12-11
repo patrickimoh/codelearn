06abe152b2717ce4530cb41f1e170402270c212e
Patrick E Imoh <patrickimoh@gmail.com>
2020-12-11 12:44:25 +0100

    new commit

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# youtuber = "Patrick E Imoh"
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \n I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
